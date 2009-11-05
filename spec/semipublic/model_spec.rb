be226507f97522ee3a329a55cb332f7bb9ed8ba6
Blake Gentry <blake@ententecorp.com>
2009-11-04 20:31:21 -0800

    Added specs for behavior of DataMapper::Model.append_extensions and .append_inclusions

---
+++ spec/semipublic/model_spec.rb
@@ -0,0 +1,82 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
+
+describe DataMapper::Model do
+  
+  it { should respond_to(:append_inclusions) }
+  
+  describe '.append_inclusions' do
+    module ::Inclusions
+      def new_method
+        true
+      end
+    end
+    
+    describe 'before the model is defined' do
+      before(:all) do
+        DataMapper::Model.append_inclusions(Inclusions)
+        
+        class ::User
+          include DataMapper::Resource
+          property :id, Serial
+        end
+      end
+      
+      it 'should respond to :new_method' do
+        User.new.should respond_to(:new_method)
+      end
+    end
+    
+    describe 'after the model is defined' do
+      before(:all) do
+        class ::User
+          include DataMapper::Resource
+          property :id, Serial
+        end
+        DataMapper::Model.append_inclusions(Inclusions)
+      end
+      
+      it 'should respond to :new_method' do
+        User.new.should respond_to(:new_method)
+      end
+    end
+  end
+  
+  it { should respond_to(:append_extensions) }
+  
+  describe '.append_extensions' do
+    module ::Extensions
+      def new_method
+        true
+      end
+    end
+    
+    describe 'before the model is defined' do
+      before(:all) do
+        DataMapper::Model.append_extensions(Extensions)
+        
+        class ::User
+          include DataMapper::Resource
+          property :id, Serial
+        end
+      end
+      
+      it 'should respond to :new_method' do
+        User.should respond_to(:new_method)
+      end
+    end
+    
+    describe 'after the model is defined' do
+      before(:all) do
+        class ::User
+          include DataMapper::Resource
+          property :id, Serial
+        end
+        DataMapper::Model.append_inclusions(Extensions)
+      end
+      
+      it 'should respond to :new_method' do
+        User.should respond_to(:new_method)
+      end
+    end
+  end
+end
